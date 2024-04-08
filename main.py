919632a5d411d3e5b6b4514b19622edd2cbc55ca
elisa.michelet <elisa.michelet@bnu.fr>
2024-04-08 11:52:34 +0200

    going back to hello world

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 # main.py
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI
 
 app = FastAPI() # This is what will be refrenced in config
 
 @app.get("/fapi")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.post("/uploadfiles/")
-async def create_upload_files(files: list[UploadFile]):
-    return {"filenames": [file.filename for file in files]}
-

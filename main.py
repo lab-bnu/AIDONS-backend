328e6e3c05e5d9b5d08b32ae9927c98b2d8dc820
elisa.michelet <elisa.michelet@bnu.fr>
2024-04-08 15:19:57 +0200

    functionnal uploadfile POST

--- main.py
+++ main.py
@@ -6,26 +6,18 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 
-@app.post("/files/")
-async def create_files(files: Annotated[list[bytes], File()]):
-    return {"file_sizes": [len(file) for file in files]}
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
 
-
-@app.post("/uploadfiles/")
-async def create_upload_files(files: list[UploadFile]):
-    return {"filenames": [file.filename for file in files]}
 
 
 @app.get("/")
 async def main():
     content = """
 <body>
-<form action="/files/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
+<form action="/uploadfile/" enctype="multipart/form-data" method="post">
+<input name="file" type="file" multiple>
 <input type="submit">
 </form>
 </body>

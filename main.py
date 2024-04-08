592c50be2e20f0654f49835dd54f42d7b8ff995b
elisa.michelet <elisa.michelet@bnu.fr>
2024-04-08 11:43:41 +0200

    only uploadfile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile
 from fastapi.responses import HTMLResponse
 from typing import Annotated
 
@@ -8,10 +8,6 @@ app = FastAPI() # This is what will be refrenced in config
 @app.get("/fapi")
 async def root():
     return {"message": "Hello World"}
-
-@app.post("/files/")
-async def create_files(files: Annotated[list[bytes], File()]):
-    return {"file_sizes": [len(file) for file in files]}
 
 
 @app.post("/uploadfiles/")

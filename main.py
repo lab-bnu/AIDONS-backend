084f0a382e3c2582b9e0b483960feca932a6faf9
elisa.michelet <elisa.michelet@bnu.fr>
2024-04-09 16:01:30 +0200

    adding barcode reader

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from typing import Annotated
-
-from fastapi import FastAPI, File, UploadFile
+from io import BytesIO
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+
+from PIL import Image
+from pyzbar.pyzbar import decode, ZBarSymbol
 
 app = FastAPI()
 
@@ -21,18 +23,31 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def read_image(image_encoded):
+    pil_image = Image.open(BytesIO(image_encoded))
+    return pil_image
+
 
 @app.post("/extractinfo/")
 async def create_extract_info(file: UploadFile):
     return {"filename": file.filename, "title": "Mon super livre", "author" : "Arthur Le Best", "year" : "2024", "ISBN" : "9782410000757"}
 
+@app.post("/barcode/")
+async def read_barcode(file: UploadFile):
+    img = read_image(file.file.read()) # PIL Image
+    decoded_list = decode(img)
+    print(decoded_list)
+    if len(decoded_list) > 0: 
+        return {"data": decoded_list[0].data,  "type": decoded_list[0].type, "rect": decoded_list[0].rect, "quality": decoded_list[0].quality, "orient": decoded_list[0].orientation}
+    else:
+        raise HTTPException(status_code=404, detail="Barcode not found")
 
 
 @app.get("/")
 async def main():
     content = """
 <body>
-<form action="/extractinfo/" enctype="multipart/form-data" method="post">
+<form action="/barcode/" enctype="multipart/form-data" method="post">
 <input name="file" type="file" multiple>
 <input type="submit">
 </form>

7c4c155ced9c959457df639c9c21a5e095d699ba
elisa.michelet <elisa.michelet@bnu.fr>
2024-04-09 09:47:38 +0200

    allow origins

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ from typing import Annotated
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "https://lab-aidons.vercel.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/extractinfo/")

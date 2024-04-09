8afe1342f26c8e2ae930ceb141aed28ba1810224
elisa.michelet <elisa.michelet@bnu.fr>
2024-04-09 16:30:38 +0200

    add pyzbar

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def read_image(image_encoded):
 async def create_extract_info(file: UploadFile):
     return {"filename": file.filename, "title": "Mon super livre", "author" : "Arthur Le Best", "year" : "2024", "ISBN" : "9782410000757"}
 
+"""
 @app.post("/barcode/")
 async def read_barcode(file: UploadFile):
     img = read_image(file.file.read()) # PIL Image
@@ -41,13 +42,13 @@ async def read_barcode(file: UploadFile):
         return {"data": decoded_list[0].data,  "type": decoded_list[0].type, "rect": decoded_list[0].rect, "quality": decoded_list[0].quality, "orient": decoded_list[0].orientation}
     else:
         raise HTTPException(status_code=404, detail="Barcode not found")
-
+"""
 
 @app.get("/")
 async def main():
     content = """
 <body>
-<form action="/barcode/" enctype="multipart/form-data" method="post">
+<form action="/extractinfo/" enctype="multipart/form-data" method="post">
 <input name="file" type="file" multiple>
 <input type="submit">
 </form>
